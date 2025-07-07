41cceb491c79187e606238aab08aacf664ebca3d
Jonatas Peclat <J.onatas.P@hotmail.com>
2025-07-07 20:49:21 -0300

    Update Show-Menu.ps1
    Version: 20250707.02
    + Added more arguments, for more customization of the menu.
    + Added option to start with default option.
    + Removed ID in the choices options. Returning the ID from the array. This makes usage simpler.

--- Show-Menu.ps1
+++ Show-Menu.ps1
@@ -3,34 +3,60 @@ function Show-Menu {
 	.SYNOPSIS
 		Generates a dynamic console menu featuring a list of options, allowing users to
 		navigate and select choices using their keyboard arrows.
+
 	.DESCRIPTION
-		The Show-Menu function is used to display a dynamic menu in the console. It takes
-		a title and a list of options as parameters. The title is optional and defaults to
-		"Please make a selection...". The list of options is mandatory. The function will
-		display the title in green, followed by the list of options. The user can then make
-		a selection from the options provided.
+		title  : [string/optional]
+			Title for the menu.
+			Defaults: "Please make a selection..."
+		caption: [string/optional]
+			Menu caption (or title).
+			Defaults: ""
+		choices: [array/required]
+			List of choices to display in the menu.
+		yselect: [string/optional]
+			Text to display when the cursor is on the selected option.
+			Defaults: " 🡢 "
+		nselect: [string/optional]
+			Text to display when the cursor is not on the selected option.
+			Defaults: "   "
+		default: [int/optional]
+			Index of the default option selected from the choices.
+			Don't forget that every array starts counting from zero.
+			Defaults: 0
+
 	.EXAMPLE
-		$MenuData += [PSCustomObject]@{Id = 1; DisplayName = "Menu Option 1"}, `
-		             [PSCustomObject]@{Id = 2; DisplayName = "Menu Option 2"}, `
-		             [PSCustomObject]@{Id = 3; DisplayName = "Menu Option 3"}
-		Show-Menu -DynamicMenuTitle "Main Menu" -DynamicMenuList $MenuData
 		This example shows how to use the Show-Menu function to display a menu with a custom title and three options.
+
+		$choice = show-menu -title "Main Menu" -caption "Select an option." -default 2 -choices (
+			"Menu Option 1",
+			"Menu Option 2",
+			"Menu Option 3"
+		)
+
 	.NOTES
 		Version: 20231115.01
-		Author:  Ryan Dunton https://github.com/ryandunton
+		Author Creator: Ryan Dunton https://github.com/ryandunton
+
+		Contributors
+			- F4Jonatas
+
+		Version: 20250707.02
+		+ Added more arguments, for more customization of the menu.
+		+ Added option to start with default option.
+		+ Removed ID in the choices options. Returning the ID from the array. This makes usage simpler.
 	#>
 
 	[cmdletbinding()]
 	param (
 		[parameter( mandatory = $false )]
 		[string]
-		$caption = "Please make a selection...",
+		$title = "Please make a selection...",
 		[parameter( mandatory = $false )]
 		[string]
-		$message = "",
-		[parameter( mandatory = $false )]
+		$caption = "",
+		[parameter( mandatory = $true )]
 		[array]
-		$dynamicmenulist,
+		$choices,
 		[parameter( mandatory = $false )]
 		[string]
 		$yselect = "$([char]27)[1m 🡢 $([char]27)[0m",
@@ -43,15 +69,25 @@ function Show-Menu {
 	)
 
 	begin {
-		write-host "$([char]27)[1m$caption$([char]27)[0m"
+		if ( $default -gt $choices.count -1 ) {
+			$default = $choices.count -1
+		}
+		elseif ( $default -lt 0 ) {
+			$default = 0
+		}
+		else {
+			$default = $default
+		}
 
-		if ( $message -ne "" ) {
-			write-host "$([char]27)[1;30m$message$([char]27)[0m"
+		write-host "$([char]27)[1m$title$([char]27)[0m"
+
+		if ( $caption -ne "" ) {
+			write-host "$([char]27)[1;30m$caption$([char]27)[0m"
 		}
 
 		# Create space for the menu
 		$index = 0
-		while ( $index -lt $dynamicmenulist.count ) {
+		while ( $index -lt $choices.count ) {
 			$index++
 			write-host ""
 		}
@@ -59,62 +95,62 @@ function Show-Menu {
 
 	process {
 		# Set initial selection index
-		$selectedvalueindex = 0
-		$break = 0
+		$selected = $default
+		$finally  = $false
+
 
 		# Display the menu and handle user input
-		while ( $break -ne $true ) {
+		while ( $finally -ne $true ) {
 			# Move cursor to top of menu area
-			[console]::setcursorposition( 0, [console]::cursortop - $dynamicmenulist.count )
+			[console]::setcursorposition( 0, [console]::cursortop - $choices.count )
 
-			for ( $i = 0; $i -lt $dynamicmenulist.count; $i++ ) {
-				if ( $i -eq $selectedvalueindex ) {
-					write-host "$yselect $( $dynamicmenulist[$i] )" -nonewline
+			for ( $index = 0; $index -lt $choices.count; $index++ ) {
+				if ( $index -eq $selected ) {
+					write-host "$yselect $( $choices[ $index ] )" -nonewline
 
-
-					if ( $keyinfo.virtualkeycode -eq 13 -and $i -eq $selectedvalueindex ) {
+					if ( $keyinfo.virtualkeycode -eq 13 -and $index -eq $selected ) {
 						write-host " $([char]27)[0;32mSelected$([char]27)[0m" -nonewline
-						$break = $true
+						$finally = $true
 					}
-					if ( $i -eq $default ) {
+					if ( $index -eq $default ) {
 						write-host " $([char]27)[1;30mDefault$([char]27)[0m" -nonewline
 					}
 				}
 
 				else {
-					write-host "$nselect $( $dynamicmenulist[$i] )" -nonewline
+					write-host "$nselect $( $choices[ $index ] )" -nonewline
+
+					if ( $index -eq $default ) {
+						write-host " $([char]27)[1;30mDefault$([char]27)[0m" -nonewline
+					}
 				}
 
 				# Clear any extra characters from previous lines
-				$spacestoclear = [math]::max( 0, ( $dynamicmenulist[0].length - $dynamicmenulist[$i].length ))
+				$spacestoclear = [math]::max( 0, ( $choices[0].length - $choices[ $index ].length ))
 				write-host ( ' ' * $spacestoclear ) -nonewline
 				write-host ''
 			}
 
 			# Get user input
-			# It will exit the loop as soon as the key is lifted
-			if ( $keyinfo.virtualkeycode -eq 13 ) {
-				$keyinfo = $host.ui.rawui.readkey( 'noecho, includekeyup' )
-			}
-			else {
+			# It will exit the loop as soon as the key is lifted.
+			if ( $finally -ne $true ) {
 				$keyinfo = $host.ui.rawui.readkey( 'noecho, includekeydown' )
-			}
 
-			# Process arrow key input
-			switch ( $keyinfo.virtualkeycode ) {
-				38 {  # Up arrow
-					$selectedvalueindex = [math]::max( 0, $selectedvalueindex - 1 )
-				}
-				40 {  # Down arrow
-					$selectedvalueindex = [math]::min( $dynamicmenulist.count - 1, $selectedvalueindex + 1 )
+				# Process arrow key input
+				switch ( $keyinfo.virtualkeycode ) {
+					38 {  # Up arrow
+						$selected = [math]::max( 0, $selected - 1 )
+					}
+					40 {  # Down arrow
+						$selected = [math]::min( $choices.count - 1, $selected + 1 )
+					}
 				}
 			}
 		}
-
-		$selectedvalue = $dynamicmenulist[ $selectedvalueindex ]
 	}
 
 	end {
-		return $selectedvalueindex
+		# $selectedvalue = $choices[ $selected ]
+		return $selected
 	}
 }
